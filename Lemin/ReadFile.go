816c34fa6f0615a4918997a44cecdcfc8cbd49b4
marikh <maryamkheyrkhah@gmail.com>
2022-11-28 18:25:22 +0200

    some changes

--- Lemin/ReadFile.go
+++ Lemin/ReadFile.go
@@ -4,36 +4,62 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+var FileSlice = []string{}
+
+//var numberOfAnts int
+
 func ReadFile(filename string) ([]string, error) {
-	file, err := os.Open(filename)
+	fileAddress := "examples/" + filename
+	file, err := os.Open(fileAddress)
 	if err != nil {
 		return nil, err
 	}
-
 	scanner := bufio.NewScanner(file)
 	// iterate over lines of file
-	i := 0
-	numberOfAnts := scanner.Text()
-	fmt.Println("numberOfAnts", numberOfAnts)
+
 	for scanner.Scan() {
-		fmt.Println(i, scanner.Text())
-		if scanner.Text() == "##start" {
-			fmt.Println("start")
-		} else if scanner.Text() == "##end" {
-			fmt.Println("end")
-		}
-		i++
+		FileSlice = append(FileSlice, scanner.Text())
 	}
 
+	fmt.Println("file is", FileSlice)
 	if err := scanner.Err(); err != nil {
 		file.Close()
 
 	}
-
+	Makefarm()
 	err = file.Close() // proper close instead of defer which apparently causes bugs
 
 	return nil, err
 
+}
+
+func Makefarm() {
+	//var err error
+	numberOfAnts, err := strconv.Atoi(FileSlice[0])
+	if err != nil {
+		fmt.Println("fuck")
+	}
+	if FileSlice[1] == "##start" {
+		start := CreateRoom(FileSlice[2])
+
+		fmt.Println("start room is :", start)
+
+	}
+}
+func CreateRoom(roomStr string) room {
+	startStr := strings.Split(roomStr, " ")
+	x, err1 := strconv.Atoi(startStr[1])
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	y, err2 := strconv.Atoi(startStr[2])
+	if err2 != nil {
+		fmt.Println(err1)
+	}
+	room := SetRoom(startStr[0], x, y)
+	return room
 }
--- Lemin/room.go
+++ Lemin/room.go
@@ -2,4 +2,14 @@ package lemin
 
 type room struct {
 	name string
+	x    int
+	y    int
+}
+
+func SetRoom(room_name string, room_x int, room_y int) room {
+	return room{
+		name: room_name,
+		x:    room_x,
+		y:    room_y,
+	}
 }
